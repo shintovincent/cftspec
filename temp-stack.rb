47b1fe7d15cf48ea22846aff75b7d91799a2e9e4
Shinto Vincent <shinto.vincent@hitachivantara.com>
2020-11-26 15:48:07 +0530

    add test case

--- temp-stack.rb
+++ temp-stack.rb
@@ -4,4 +4,5 @@ data = JSON.parse(File.read('output.json'))
 
 describe aws_security_group(data['InstanceSecurityGroup']) do
   it { should exist }
+  it { should allow_in(port: 22, ipv4_range: '103.99.206.132/32') }
 end
